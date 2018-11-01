8338c7fde7213060841594e4746db1c8a40b78ef
vipul-gorasiya <39234040+vipul-gorasiya@users.noreply.github.com>
2018-11-01 16:59:41 +0530

    Added eureka server

--- CustomerService/src/main/java/com/vipul/CustomerServiceApplication.java
+++ CustomerService/src/main/java/com/vipul/CustomerServiceApplication.java
@@ -1,34 +1,20 @@
 package com.vipul;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableWebMvc
+@EnableSwagger2
 @EnableJpaRepositories
 @SpringBootApplication
-@EnableSwagger2
-public class CustomerServiceApplication implements WebMvcConfigurer {
+@EnableEurekaClient
+public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-	}
-	
-	@Bean
-	public ModelMapper modelMapper() {
-	    return new ModelMapper();
-	}
 }
