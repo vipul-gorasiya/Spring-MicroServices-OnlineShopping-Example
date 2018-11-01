a780b6bc391fca2c4d4102b080f2cacc1d3fdac0
vipul-gorasiya <39234040+vipul-gorasiya@users.noreply.github.com>
2018-11-01 17:06:43 +0530

    Update EurekaServer/src/main/java/com/vipul/EurekaServerApplication.java

--- EurekaServer/src/main/java/com/vipul/EurekaServerApplication.java
+++ EurekaServer/src/main/java/com/vipul/EurekaServerApplication.java
@@ -2,9 +2,9 @@ package com.vipul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaClient
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
